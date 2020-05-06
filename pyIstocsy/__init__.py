38642a930c3f609a6d8c44413f6389fa3e8cd3f7
carolinesands <caroline.sands01@imperial.ac.uk>
2020-05-06 15:06:47 +0100

    Delete prior to v2.0.0 update

--- pyIstocsy/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-The `nPYc-Toolbox <https://github.com/phenomecentre/nPYc-Toolbox>`_ defines objects for representing, and implements functions to manipulate and display, metabolic profiling datasets.
-"""
-__version__ = '1.0.0'
-
-from .runISTOCSY import main as runISTOCSY
-
-__all__ = ['runISTOCSY']
